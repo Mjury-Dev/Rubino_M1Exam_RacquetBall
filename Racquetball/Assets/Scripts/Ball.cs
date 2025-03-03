d02ce3e8e386788d0fde620323b0ea9590b03d06
Mjury-Dev <mikejurirubino@gmail.com>
2025-03-03 19:56:05 +0800

    Racquetball Game

---
+++ Racquetball/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float ballInitVelocity = 300f;
+    private Rigidbody rb;
+    private bool ballInPlay = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !ballInPlay)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitVelocity, ballInitVelocity, 0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Racquet"))
+        {
+            GameManager.instance.AddCount();
+        }
+    }
+}
---
+++ Racquetball/Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class GameManager : MonoBehaviour
+{
+    public float resetDelay = 1f;
+    public GameObject spikes;
+    public GameObject racquet;
+    public GameObject ball;
+    public GameObject deathParticles;
+    public GameObject frontWall;
+    public GameObject backWall;
+    private int bounceCount;
+
+   
+    public TextMesh scoreTextMesh;
+
+    public static GameManager instance = null;
+
+    
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        frontWall = GameObject.Find("FrontWall");
+        backWall = GameObject.Find("BackWall");
+        racquet = GameObject.Find("Racquet");
+        ball = GameObject.Find("Ball");
+        spikes = GameObject.Find("Spikes");
+
+        deathParticles = GameObject.Find("DeathParticles");
+
+        bounceCount = 0;
+
+        Setup();
+        UpdateScoreText();
+    }
+
+    public void Setup()
+    {
+        spikes.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (bounceCount > 5)
+        {
+            spikes.SetActive(true);
+        }
+    }
+
+    public void AddCount()
+    {
+        bounceCount++;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreTextMesh != null)
+        {
+            scoreTextMesh.text = "Score: " + bounceCount;
+        }
+        else
+        {
+            Debug.LogError("Score TextMesh not assigned in GameManager!");
+        }
+    }
+
+    public void Reset()
+    {
+        // Fix applied here — using SceneManager instead of obsolete Application.loadedLevel
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void DeadBall()
+    {
+        Destroy(racquet);
+        Instantiate(Resources.Load("DeathParticles"), ball.transform.position, Quaternion.identity);
+        Invoke("Reset", resetDelay);
+    }
+}
